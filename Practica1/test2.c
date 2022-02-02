f185ae93e898dc03e15d2c6f4f622254ebc88256
Sir Bigotes <83607370+jimePCR@users.noreply.github.com>
2022-02-01 19:09:44 -0600

    Update test2.c

--- Practica1/test2.c
+++ Practica1/test2.c
@@ -1,14 +1,49 @@
 //jeje
-#include <stdio.h>
-
-struct Nodo{
-  
-};
+#include <iostream>
+#include <string>
+using namespace std;
+/*struct Nodo{
+  String nom;
+  //int inicio;
+  int tam;
+  struct Nodo *sig;
+};*/
+int menu(){
+  int op;
+  cout << "---MENU---" << endl;
+  cout << "1.Agregar proceso"<< endl;
+  cout << "2.Eliminar proceso" << endl;
+  cout << "3.Salir" << endl;
+  cin >> op;
+  return op;
+}
 int main(int argc, char const *argv[]) {
-  int a = 1;
-  int b = 2;
-  int c = a+b;
-  printf("Hola mundo\n");
-  printf("%d\n", c);
+  string nom;
+  int longitud, op;
+
+  do{
+    op = menu();
+
+    switch (op) {
+      case 1:
+        cout << "Dame el nombre: " << '\n';
+        cin >> nom;
+        cout << "Dame la longitud (EN BYTES): "<<'\n';
+        cin >> longitud;
+
+        longitud = longitud / 1024; //Longitud en kb
+
+      break;
+      case 2:
+      break;
+      case 3:
+        cout << "Görüşürüz!" <<endl;
+      break;
+      default:
+        cout << "Opcion incorrecta" << endl;
+      break;
+    }
+
+  }while(op != 3);
   return 0;
 }
