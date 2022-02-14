3cae0f313acbca635dbd8c8024834acb1472d7b7
x0chipa <68248983+x0chipa@users.noreply.github.com>
2022-02-14 17:52:16 -0600

    es una clase xD

--- Practica3/src/practica3/Main.java
+++ Practica3/src/practica3/Main.java
@@ -6,7 +6,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo hermoso xD");
     }
     
 }
---
+++ Practica3/src/practica3/clase.java
@@ -0,0 +1,4 @@
+public class clase {
+    // no se como hacer una clase si no me ayuda netbeans xD
+    
+}
