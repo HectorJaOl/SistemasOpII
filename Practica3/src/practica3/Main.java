01fcb77bec394d3729d990a33c56dae33becc498
x0chipa <68248983+x0chipa@users.noreply.github.com>
2022-02-14 18:44:30 -0600

    ola

--- Practica3/src/practica3/Main.java
+++ Practica3/src/practica3/Main.java
@@ -1,10 +1,15 @@
 package practica3;
+import java.io.File;
+import java.util.Scanner;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo hermoso xD");
     }
+
 
     int DireccionVirtual;
     int TamanoPalabra, numeroBitsPagina1Nivel, numeroBitsPagina2Nivel, numeroBitsMarcoPagina;
