48bbc0fad89d84c870c2689bad1569a2824c50cb
Sir Bigotes <83607370+jimePCR@users.noreply.github.com>
2022-02-14 18:52:41 -0600

    Update MemoriaVirtual.java

--- Practica3/src/practica3/MemoriaVirtual.java
+++ Practica3/src/practica3/MemoriaVirtual.java
@@ -15,6 +15,93 @@ public class MemoriaVirtual {
         this.numeroBitsPagina2Nivel = numeroBitsPagina2Nivel;
         this.numeroBitsMarcoPagina = numeroBitsMarcoPagina;
     }
+
+    public int getDireccionVirtual() {
+        return DireccionVirtual;
+    }
+
+    public void setDireccionVirtual(int DireccionVirtual) {
+        this.DireccionVirtual = DireccionVirtual;
+    }
+
+    public int getTamanoPalabra() {
+        return TamanoPalabra;
+    }
+
+    public void setTamanoPalabra(int TamanoPalabra) {
+        this.TamanoPalabra = TamanoPalabra;
+    }
+
+    public int getNumeroBitsPagina1Nivel() {
+        return numeroBitsPagina1Nivel;
+    }
+
+    public void setNumeroBitsPagina1Nivel(int numeroBitsPagina1Nivel) {
+        this.numeroBitsPagina1Nivel = numeroBitsPagina1Nivel;
+    }
+
+    public int getNumeroBitsPagina2Nivel() {
+        return numeroBitsPagina2Nivel;
+    }
+
+    public void setNumeroBitsPagina2Nivel(int numeroBitsPagina2Nivel) {
+        this.numeroBitsPagina2Nivel = numeroBitsPagina2Nivel;
+    }
+
+    public int getNumeroBitsMarcoPagina() {
+        return numeroBitsMarcoPagina;
+    }
+
+    public void setNumeroBitsMarcoPagina(int numeroBitsMarcoPagina) {
+        this.numeroBitsMarcoPagina = numeroBitsMarcoPagina;
+    }
+
+    public int getBitsReferida() {
+        return bitsReferida;
+    }
+
+    public void setBitsReferida(int bitsReferida) {
+        this.bitsReferida = bitsReferida;
+    }
+
+    public int getBitsModificada() {
+        return bitsModificada;
+    }
+
+    public void setBitsModificada(int bitsModificada) {
+        this.bitsModificada = bitsModificada;
+    }
+
+    public int getBitsPermisos() {
+        return bitsPermisos;
+    }
+
+    public void setBitsPermisos(int bitsPermisos) {
+        this.bitsPermisos = bitsPermisos;
+    }
+
+    public int getBitPresenteAusente() {
+        return bitPresenteAusente;
+    }
+
+    public void setBitPresenteAusente(int bitPresenteAusente) {
+        this.bitPresenteAusente = bitPresenteAusente;
+    }
     
-    
+    public int numPagina(){
+       return (int) Math.pow(2, numeroBitsPagina1Nivel) * (int) Math.pow(2, numeroBitsPagina2Nivel);
+       
+    }
+    public void tamMemoriaFisica(){
+        
+    }
+    public int tamPagina(){
+        int desplazamiento;
+        desplazamiento = numeroBitsPagina1Nivel - numeroBitsPagina2Nivel;
+        desplazamiento = desplazamiento/TamanoPalabra;
+        return (int) Math.pow(2, desplazamiento);
+    }
+    public String toString(){
+        return "Tam Memoria Virtual: "+"Tam Memoria Fisica"+"Tam de pagina: "+tamPagina();
+    }
 }
