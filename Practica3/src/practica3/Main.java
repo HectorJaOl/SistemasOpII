bbf9916271f4ea34b0033d74f11d3b2d16808238
x0chipa <68248983+x0chipa@users.noreply.github.com>
2022-02-14 18:52:15 -0600

    hola
    xD

--- Practica3/src/practica3/Main.java
+++ Practica3/src/practica3/Main.java
@@ -7,15 +7,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo hermoso xD");
+        
+        int a = 10;
     }
 
 
-    int DireccionVirtual;
+    /*int DireccionVirtual;
     int TamanoPalabra, numeroBitsPagina1Nivel, numeroBitsPagina2Nivel, numeroBitsMarcoPagina;
     int bitsReferida, bitsModificada, bitsPermisos, bitPresenteAusente;
 
     //archivo tabla de pagina
-
+*/
     
 }
