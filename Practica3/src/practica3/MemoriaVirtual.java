6efc887b8a821753d60f39acf47afef0658a91df
Sir Bigotes <83607370+jimePCR@users.noreply.github.com>
2022-02-14 18:30:54 -0600

    Update MemoriaVirtual.java

--- Practica3/src/practica3/MemoriaVirtual.java
+++ Practica3/src/practica3/MemoriaVirtual.java
@@ -6,5 +6,15 @@ public class MemoriaVirtual {
     private int numeroBitsPagina1Nivel;
     private int numeroBitsPagina2Nivel;
     private int numeroBitsMarcoPagina;
-    private int bitsReferida; bitsModificada; bitsPermisos; bitPresenteAusente;
+    private int bitsReferida, bitsModificada, bitsPermisos, bitPresenteAusente;
+
+    public MemoriaVirtual(int DireccionVirtual, int TamanoPalabra, int numeroBitsPagina1Nivel, int numeroBitsPagina2Nivel, int numeroBitsMarcoPagina) {
+        this.DireccionVirtual = DireccionVirtual;
+        this.TamanoPalabra = TamanoPalabra;
+        this.numeroBitsPagina1Nivel = numeroBitsPagina1Nivel;
+        this.numeroBitsPagina2Nivel = numeroBitsPagina2Nivel;
+        this.numeroBitsMarcoPagina = numeroBitsMarcoPagina;
+    }
+    
+    
 }
