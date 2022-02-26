6f9f402878f654074d00aa7c7e98da58afe53df1
Sir Bigotes <83607370+jimePCR@users.noreply.github.com>
2022-02-26 15:37:16 -0600

    Paso 1 listo

--- Practica 4/Practica4.c
+++ Practica 4/Practica4.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
-
-struct Nodo
+#include <stdlib.h>
+#include <string.h>
+int matriz[2][25];
+typedef struct Nodo
 {
-  char nombreProceso;
+  char nombreProceso[10];
   int numPaginas;
   int desplazamiento[2][25];
-  int numReferencias;
-  struct Nodo *sig;
-};
+  struct Nodo *siguiente;
+} Nodo;
+
+Nodo *cabeza = NULL;
+
+Nodo *crearNodo(char *nombreProceso, int numPaginas)
+{
+
+  Nodo *nodo = (Nodo *)malloc(sizeof(Nodo));
+
+  strcpy(nodo->nombreProceso, nombreProceso);
+  nodo->numPaginas = numPaginas;
+
+  // Agregar/Llenar la matriz
+  /*for(int i=0; i<25; i++){
+    matriz[0][i] = desplazamiento[0][i];
+    matriz[0][i] = desplazamiento[0][i];
+  }*/
+
+  nodo->siguiente = NULL;
+  return nodo;
+}
+
+void insertarNodo(char *nombreProceso, int numPaginas)
+{
+  Nodo *nodo = crearNodo(nombreProceso, numPaginas);
+  if (cabeza == NULL)
+    cabeza = nodo;
+  else
+  {
+    Nodo *puntero = cabeza;
+
+    while (puntero->siguiente)
+      puntero = puntero->siguiente;
+
+    puntero->siguiente = nodo;
+  }
+}
+
+void mostrarLista()
+{
+  printf("Nombre | Paginas\n\n");
+
+  Nodo *aux = cabeza;
+
+  while (aux != NULL)
+  {
+    printf("[%s | %d]->", aux->nombreProceso, aux->numPaginas);
+    aux = aux->siguiente;
+  }
+  printf("\n\n");
+}
 
 int menu()
 {
@@ -17,64 +68,86 @@ int menu()
   printf("2. Despachar proceso\n");
   printf("3. Salir\n");
   printf("Opcion: ");
-  scanf("%d\n", &op);
+  scanf("%d", &op);
 
   return op;
 }
 
 int main(int argc, char const *argv[])
 {
-  int opcion;
+  int opcion, cont;
   char nombreProceso[100], termina;
   int numPaginas, numPag, desplazamiento;
   int tamPagina, tamMemoriaVirtual, tamMemoriaFisica;
 
   // Nodo procesos;
 
-  printf("Tamano de Pagina: ");
-  scanf("%d\n", &tamPagina);
+  printf("Tamano de Pagina en bytes: ");
+  scanf("%d", &tamPagina);
 
-  printf("Tamano Memoria Virtual: ");
-  scanf("%d\n", &tamMemoriaVirtual);
+  printf("Tamano Memoria Virtual en bytes: ");
+  scanf("%d", &tamMemoriaVirtual);
 
-  printf("Tamano Memoria Fisica: ");
-  scanf("%d\n", &tamMemoriaFisica);
+  printf("Tamano Memoria Fisica en bytes: ");
+  scanf("%d", &tamMemoriaFisica);
 
-  opcion = menu();
-  switch (opcion)
+  do
   {
-  case 1:
-    printf("Nombre del proceso: \n");
+  opcion = menu();
 
-    fflush(stdin);
-    gets(nombreProceso);
-
-    do
+    switch (opcion)
     {
-      printf("Numero de Paginas: \n");
-      scanf("%d\n", &numPaginas);
-    } while (numPaginas < 1);
+    case 1:
+      printf("Nombre del proceso: \n");
 
-    do
-    {
-      printf("Numero Pagina: ");
-      scanf("%d\n", &numPag);
+      setbuf(stdin, NULL);
+      scanf("%s", nombreProceso);
 
-      printf("Desaplazamiento: ");
-      scanf("%d\n", &desplazamiento);
-
-      printf("Desea meter otro? [S/n]:    ");
-      scanf("%c\n", termina);
-      if (termina == 'n' && termina == 'N')
+      do
       {
-        break;
-      }
-    } while (1);
+        printf("Numero de Paginas: \n");
+        scanf("%d", &numPaginas);
+      } while (numPaginas < 1);
 
-    break;
-  case 2:
-    break;
-  }
+      cont = 0;
+      do{
+        do
+        {
+          printf("Numero Pagina: ");
+          scanf("%d", &numPag);
+
+          if (numPag > numPaginas)
+          {
+            printf("\nPagina no disponible\n");
+          }
+
+        } while (numPag > numPaginas);
+
+        printf("Desplazamiento: ");
+        scanf("%d", &desplazamiento);
+
+        // agregarMatriz(numPag,desplazamiento);
+        matriz[0][cont] = numPag;
+        matriz[1][cont] = desplazamiento;
+
+        cont++;
+        printf("Desea meter otro? [S/n]:    ");
+        setbuf(stdin, NULL);
+        scanf("%c", &termina);
+        if (termina == 78 || termina == 110)
+        {
+          break;
+        }
+      }while(1);
+      insertarNodo(nombreProceso, numPaginas);
+      break;
+    case 2:
+      break;
+    case 3:
+      mostrarLista();
+      break;
+    }
+  } while (opcion != 3);
 
   return 0;
 }
