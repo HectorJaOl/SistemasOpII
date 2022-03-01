e7c987abbcacaad377174f7d9471b7fa067e5bc8
x0chipa <68248983+x0chipa@users.noreply.github.com>
2022-02-28 19:05:20 -0600

    Creo que ya quedo la matriz
    olis

--- Practica 4/Practica4.c
+++ Practica 4/Practica4.c
@@ -1,226 +1,219 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int matriz[2][25];
-
-typedef struct Nodo{
-  char nombreProceso[10];
-  int numPaginas;
-  int desplazamiento[25][2];
-  int numReferencias;
-  struct Nodo *sig;
-} Nodo;
+int matriz[25][2]; //[x,x],[x,x],,[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x],[x,x]
 
 typedef struct Nodo
 {
-  char nombreProceso[10];
-  int numPaginas;
-  int desplazamiento[2][25];
-  struct Nodo *siguiente;
+    char nombreProceso[10];
+    int numPaginas;
+    int numReferencias;
+    int desplazamiento[25][2];
+    struct Nodo *siguiente;
 } Nodo;
 
 Nodo *cabeza = NULL;
 
-Nodo *crearNodo(char *nombreProceso, int numPaginas)
+Nodo *crearNodo(char *nombreProceso, int numPaginas, int referencias)
 {
+    Nodo *nodo = (Nodo *)malloc(sizeof(Nodo));
 
-  Nodo *nodo = (Nodo *)malloc(sizeof(Nodo));
+    strcpy(nodo->nombreProceso, nombreProceso);
+    nodo->numPaginas = numPaginas;
 
-  strcpy(nodo->nombreProceso, nombreProceso);
-  nodo->numPaginas = numPaginas;
+    nodo->numReferencias = referencias;
 
-  // Agregar/Llenar la matriz
-  /*for(int i=0; i<25; i++){
-    matriz[0][i] = desplazamiento[0][i];
-    matriz[0][i] = desplazamiento[0][i];
-  }*/
+    // Agregar/Llenar la matriz
+    for (int i = 0; i < 25; i++)
+    {
+        nodo->desplazamiento[i][0] = matriz[i][0];
+        nodo->desplazamiento[i][1] = matriz[i][1];
+    }
 
-  nodo->siguiente = NULL;
-  return nodo;
+    nodo->siguiente = NULL;
+    return nodo;
 }
 
-void insertarNodo(char *nombreProceso, int numPaginas)
+void insertarNodo(char *nombreProceso, int numPaginas, int referencias)
 {
-  Nodo *nodo = crearNodo(nombreProceso, numPaginas);
-  if (cabeza == NULL)
-    cabeza = nodo;
-  else
-  {
-    Nodo *puntero = cabeza;
+    Nodo *nodo = crearNodo(nombreProceso, numPaginas, referencias);
+    if (cabeza == NULL)
+        cabeza = nodo;
+    else
+    {
+        Nodo *puntero = cabeza;
 
-    while (puntero->siguiente)
-      puntero = puntero->siguiente;
+        while (puntero->siguiente)
+            puntero = puntero->siguiente;
 
-    puntero->siguiente = nodo;
-  }
+        puntero->siguiente = nodo;
+    }
 }
 
 void mostrarLista()
 {
-  printf("Nombre | Paginas\n\n");
+    printf("Nombre | Paginas\n\n");
 
-  Nodo *aux = cabeza;
+    Nodo *aux = cabeza;
 
-  while (aux != NULL)
-  {
-    printf("[%s | %d]->", aux->nombreProceso, aux->numPaginas);
-    aux = aux->siguiente;
-  }
-  printf("\n\n");
-}
-
-Nodo *crearNodo(char *nombreProceso, int numPaginas, int referencias, int nReferencias[referencias][2]){
-
-  Nodo *nodo = (Nodo *)malloc(sizeof(Nodo));
-
-  strcpy(nodo->nombreProceso, nombreProceso);
-  nodo->numPaginas = numPaginas;
-
-  // Agregar/Llenar la matriz
-  nodo->desplazamiento = nReferencias[referencias][2];
-
-  nodo->numPaginas = referencias;
-
-  nodo->sig = NULL;
-  return nodo;
-}
-
-int menu(){
-  int op;
-  printf("\n----MENU----\n");
-  printf("1. Proceso nuevo\n");
-  printf("2. Despachar proceso\n");
-  printf("3. Salir\n");
-  printf("Opcion: ");
-  scanf("%d", &op);
-
-  return op;
-}
-
-int main(int argc, char const *argv[])
-{
-  int opcion, cont;
-  char nombreProceso[100], termina;
-  int numPaginas, numPag, desplazamiento;
-  int tamPagina, tamMemoriaVirtual, tamMemoriaFisica;
-
-  // Nodo procesos;
-
-  printf("Tamano de Pagina en bytes: ");
-  scanf("%d", &tamPagina);
-
-  printf("Tamano Memoria Virtual en bytes: ");
-  scanf("%d", &tamMemoriaVirtual);
-
-  printf("Tamano Memoria Fisica en bytes: ");
-  scanf("%d", &tamMemoriaFisica);
-
-  opcion = menu();
-  switch (opcion){
-  case 1:
-    printf("Nombre del proceso: \n");
-
-    setbuf(stdin, NULL);
-    scanf("%s", nombreProceso);
-
-    do{
-      printf("Numero de Paginas: \n");
-      scanf("%d", &numPaginas);
-    } while (numPaginas < 1);
-
-    cont = 0;
-    do{
-      do{
-        printf("Numero Pagina: ");
-        scanf("%d", &numPag);
-
-        if (numPag > numPaginas){
-          printf("\nPagina no disponible\n");
+    while (aux != NULL){
+        printf("[%s | %d | %d]--", aux->nombreProceso, aux->numPaginas, aux->numReferencias);
+        for (int i = 0; i < aux->numReferencias; i++){
+            printf("[%d/%d]",aux->desplazamiento[i][0],aux->desplazamiento[i][1]);
         }
+        printf("->");
+        aux = aux->siguiente;
+    }
+    printf("\n\n");
+}
 
-      } while (numPag > numPaginas);
+int menu()
+{
+    int op;
+    printf("\n----MENU----\n");
+    printf("1. Proceso nuevo\n");
+    printf("2. Despachar proceso\n");
+    printf("3. Salir\n");
+    printf("Opcion: ");
+    scanf("%d", &op);
 
-      printf("Desaplazamiento: ");
-      scanf("%d", &desplazamiento);
+    return op;
+}
 
-      // agregarMatriz(numPag,desplazamiento);
-      matriz[cont][0] = numPag;
-      matriz[cont][1] = desplazamiento;
+int main()
+{
+    int opcion, cont;
+    char nombreProceso[100], termina;
+    int numPaginas, numPag, desplazamiento;
+    int tamPagina, tamMemoriaVirtual, tamMemoriaFisica;
 
-      cont++;
-      printf("Desea meter otro? [S/n]:    ");
-      setbuf(stdin, NULL);
-      scanf("%c", &termina);
-      if (termina == 78 || termina == 110){
-        break;
-      }
-    } while (1);
-    break;
-  case 2:
-    break;
-  }
+    // Nodo procesos;
 
-  do
-  {
-  opcion = menu();
+    printf("Tamano de Pagina en bytes: ");
+    scanf("%d", &tamPagina);
 
+    printf("Tamano Memoria Virtual en bytes: ");
+    scanf("%d", &tamMemoriaVirtual);
+
+    printf("Tamano Memoria Fisica en bytes: ");
+    scanf("%d", &tamMemoriaFisica);
+
+    opcion = menu();
     switch (opcion)
     {
     case 1:
-      printf("Nombre del proceso: \n");
+        printf("Nombre del proceso: \n");
 
-      setbuf(stdin, NULL);
-      scanf("%s", nombreProceso);
+        setbuf(stdin, NULL);
+        scanf("%s", nombreProceso);
 
-      do
-      {
-        printf("Numero de Paginas: \n");
-        scanf("%d", &numPaginas);
-      } while (numPaginas < 1);
-
-      cont = 0;
-      do{
         do
         {
-          printf("Numero Pagina: ");
-          scanf("%d", &numPag);
+            printf("Numero de Paginas: \n");
+            scanf("%d", &numPaginas);
+        } while (numPaginas < 1);
 
-          if (numPag > numPaginas)
-          {
-            printf("\nPagina no disponible\n");
-          }
-
-        } while (numPag > numPaginas);
-
-        printf("Desplazamiento: ");
-        scanf("%d", &desplazamiento);
-
-        // agregarMatriz(numPag,desplazamiento);
-        matriz[0][cont] = numPag;
-        matriz[1][cont] = desplazamiento;
-
-        cont++;
-        printf("Desea meter otro? [S/n]:    ");
-        setbuf(stdin, NULL);
-        scanf("%c", &termina);
-        if (termina == 78 || termina == 110)
+        cont = 0;
+        do
         {
-          break;
-        }
-      }while(1);
-      insertarNodo(nombreProceso, numPaginas);
-      break;
+            do
+            {
+                printf("Numero Pagina: ");
+                scanf("%d", &numPag);
+
+                if (numPag > numPaginas)
+                {
+                    printf("\nPagina no disponible\n");
+                }
+
+            } while (numPag > numPaginas);
+
+            printf("Desaplazamiento: ");
+            scanf("%d", &desplazamiento);
+
+            // agregarMatriz(numPag,desplazamiento);
+            matriz[cont][0] = numPag;
+            matriz[cont][1] = desplazamiento;
+
+            cont++;
+            printf("Desea meter otro? [S/n]:    ");
+            setbuf(stdin, NULL);
+            scanf("%c", &termina);
+            if (termina == 78 || termina == 110)
+            {
+                break;
+            }
+        } while (1);
+        break;
     case 2:
-      break;
-    case 3:
-      mostrarLista();
-      break;
+        break;
     }
-  } while (opcion != 3);
 
-  for (int i = 0; i < cont; i++){
-    printf("[%d][%d]--", matriz[i][0], matriz[i][1]);
-  }
+    do
+    {
+        opcion = menu();
 
-  return 0;
+        switch (opcion)
+        {
+        case 1:
+            printf("Nombre del proceso: \n");
+
+            setbuf(stdin, NULL);
+            scanf("%s", nombreProceso);
+
+            do
+            {
+                printf("Numero de Paginas: \n");
+                scanf("%d", &numPaginas);
+            } while (numPaginas < 1);
+
+            cont = 0;
+            do
+            {
+                do
+                {
+                    printf("Numero Pagina: ");
+                    scanf("%d", &numPag);
+
+                    if (numPag > numPaginas)
+                    {
+                        printf("\nPagina no disponible\n");
+                    }
+
+                } while (numPag > numPaginas);
+
+                printf("Desplazamiento: ");
+                scanf("%d", &desplazamiento);
+
+                // agregarMatriz(numPag,desplazamiento);
+                matriz[0][cont] = numPag;
+                matriz[1][cont] = desplazamiento;
+
+                cont++;
+                printf("Desea meter otro? [S/n]:    ");
+                setbuf(stdin, NULL);
+                scanf("%c", &termina);
+                if (termina == 78 || termina == 110)
+                {
+                    break;
+                }
+            } while (1);
+            insertarNodo(nombreProceso, numPaginas, cont);
+            break;
+        case 2:
+            break;
+        case 3:
+            mostrarLista();
+            break;
+        }
+    } while (opcion != 3);
+
+    
+    
+    /*for (int i = 0; i < cont; i++)
+    {
+        printf("[%d][%d]--", matriz[i][0], matriz[i][1]);
+    }*/
+
+    return 0;
 }
