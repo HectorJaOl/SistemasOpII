681d2829103e52fe1f8f3f685254d450f006007b
Hector <89277409+HectorJaOl@users.noreply.github.com>
2022-03-05 00:03:17 -0600

    Update despachador.h

--- Practica4/despachador.h
+++ Practica4/despachador.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "lista.h"
 
+//Declaracion de funciones
 void imprimirDatosProceso (struct Nodo* proceso);
 void convertir(int decimal, int mem, int base);
 void convertirDes(int decimal, int base);
@@ -37,19 +38,25 @@ void despacha() {
 		printf ("\nAhora ejecutando el proceso: %s\n", proceso->nombreProceso);
 		
 		//DirVir
+		//Base 10
 		printf ("Pagina | Desplazamiento: \n[%d|%d]\n", proceso->direccionV[proceso->numReferencias-proceso->refRestantes][0], proceso->direccionV[proceso->numReferencias-proceso->refRestantes][1]);
+		//Base 2
 		printf ("\nDireccion Virtual Base 2:\n");
 		convertir(proceso->direccionV[proceso->numReferencias-proceso->refRestantes][0], pagMV, 2);
 		convertirDes(proceso->direccionV[proceso->numReferencias-proceso->refRestantes][1], 2);
+		//Base 16
 		printf ("\n\nDireccion Virtual Base 16:\n");
 		convertir(proceso->direccionV[proceso->numReferencias-proceso->refRestantes][0], pagMV, 16);
 		convertirDes(proceso->direccionV[proceso->numReferencias-proceso->refRestantes][1], 16);
 		
 		//DirFis
+		//Base 10
 		printf ("\n\nMarco | Desplazamiento: \n[%d|%d]\n", proceso->direccionF[proceso->numReferencias-proceso->refRestantes][0], proceso->direccionF[proceso->numReferencias-proceso->refRestantes][1]);
+		//Base 2
 		printf ("\nDireccion Fisica Base 2:\n");
 		convertir(proceso->direccionF[proceso->numReferencias-proceso->refRestantes][0], pagMF, 2);
 		convertirDes(proceso->direccionF[proceso->numReferencias-proceso->refRestantes][1], 2);
+		//Base 16
 		printf ("\n\nDireccion Fisica Base 16:\n");
 		convertir(proceso->direccionF[proceso->numReferencias-proceso->refRestantes][0], pagMF, 16);
 		convertirDes(proceso->direccionF[proceso->numReferencias-proceso->refRestantes][1], 16);
@@ -84,6 +91,9 @@ void imprimirDatosProceso (struct Nodo* proceso)
 	printf ("Numero de Referencias: %d\n", proceso->numReferencias);
 }
 
+//Convierte las paginas y marcos a base 2 y 16 
+//Y agrega los 0's necesarios para llenar la 
+//Cantidad de bits que usan las direcciones
 void convertir(int decimal, int mem, int base)
 {
 	int cociente,residuo,cont, i,j;
@@ -109,6 +119,8 @@ void convertir(int decimal, int mem, int base)
     printf(" ");
 }
 
+//Convierte el desplazamiento por separado
+//Porque aqui no hacen falta 0's
 void convertirDes(int decimal, int base)
 {
 	int cociente,residuo,cont, i,j;
@@ -129,6 +141,9 @@ void convertirDes(int decimal, int base)
     printf(" ");
 }
 
+//Convierte la cantidad de paginas de las 
+//Memorias a binario para sacar la cantidad
+//De bits necesarios
 int convMem(int decimal, int base)
 {
 	int cont=0, cociente,residuo, i,j;
